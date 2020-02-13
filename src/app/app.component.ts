63ad6c4c0b5de4d9ffcb10d2a56dcf857a5d422a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-02-13 02:37:24 -0300

    Injetando app state na aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppState } from './store/models/app-state.model';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngrx-shopping-list';
+
+  constructor(private store: Store<AppState>) {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { ShoppingReducer } from './reducers/shopping.reducer';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     StoreModule.forRoot({
       shopping: ShoppingReducer,
     }),
