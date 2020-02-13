94f8a6229be1d9b8614766030ded407088947e5a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-02-13 03:36:40 -0300

    Importação httpModule e adição actions side effects

--- src/app/store/actions/shopping.actions.ts
+++ src/app/store/actions/shopping.actions.ts
@@ -3,9 +3,16 @@ import { ShoppingItem } from '../models/shopping-item.model';
 
 export enum ShoppingActionTypes {
   ADD_ITEM = '@shopping/ADD_ITEM',
-  REMOVE_ITEM = '@shopping/REMOVE_ITEM',
   ADD_ITEM_SUCCESS = '@shopping/ADD_ITEM_SUCCESS',
   ADD_ITEM_FAILURE = '@shopping/ADD_ITEM_FAILURE',
+
+  LOAD_SHOPPING = '@shopping/LOAD_SHOPPING',
+  LOAD_SHOPPING_SUCCESS = '@shopping/LOAD_SHOPPING_SUCCESS',
+  LOAD_SHOPPING_FAILURE = '@shopping/LOAD_SHOPPING_FAILURE',
+
+  REMOVE_ITEM = '@shopping/REMOVE_ITEM',
+  REMOVE_ITEM_SUCCESS = '@shopping/REMOVE_ITEM_SUCCESS',
+  REMOVE_ITEM_FAILURE = '@shopping/REMOVE_ITEM_FAILURE',
 }
 
 export class AddItemAction implements Action {
