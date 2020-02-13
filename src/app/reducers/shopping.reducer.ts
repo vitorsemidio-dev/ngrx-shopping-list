ce3671ccc1cd79ec49bc43367e01f3479956f41a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-02-13 03:57:17 -0300

    Adionado todos os casos ao reducer

--- src/app/reducers/shopping.reducer.ts
+++ src/app/reducers/shopping.reducer.ts
@@ -11,18 +11,53 @@ const initialState: ShoppingState = {
   list: [],
   loading: false,
   error: undefined,
-}
+};
 
 export function ShoppingReducer(state: ShoppingState = initialState, action: ShoppingAction) {
   switch (action.type) {
-    case ShoppingActionTypes.ADD_ITEM_SUCCESS:
-      return {...state, list: action.payload, loading: false };
+    case ShoppingActionTypes.LOAD_SHOPPING:
+      return {
+        ...state, loading: true,
+      };
+    case ShoppingActionTypes.LOAD_SHOPPING_SUCCESS:
+      return {
+        ...state, loading: false, list: action.payload
+      };
+    case ShoppingActionTypes.LOAD_SHOPPING_FAILURE:
+      return {
+        ...state, error: action.payload, loading: false
+      };
+
+
     case ShoppingActionTypes.REMOVE_ITEM:
       return {
-        ...state,
-        list: state.list.filter(s => s.id !== action.payload.id),
-        loading: false
+        ...state, loading: true,
       };
+    case ShoppingActionTypes.REMOVE_ITEM_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        list: state.list.filter(s => s.id !== action.payload.id)
+      };
+    case ShoppingActionTypes.REMOVE_ITEM_FAILURE:
+      return {
+        ...state, error: action.payload, loading: false
+      };
+
+
+    case ShoppingActionTypes.ADD_ITEM:
+      return {
+        ...state, loading: true,
+      };
+    case ShoppingActionTypes.ADD_ITEM_SUCCESS:
+      return {
+        ...state, loading: false, list: action.payload
+      };
+    case ShoppingActionTypes.ADD_ITEM_FAILURE:
+      return {
+        ...state, error: action.payload, loading: false
+      };
+
     default:
       return state;
   }
