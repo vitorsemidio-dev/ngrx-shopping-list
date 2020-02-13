d37e9ca5cf50dc2d11bf49423c1dde139bab8231
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-02-13 02:41:31 -0300

    Observable para ShoppingItem e inicialização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
-import { AppState } from './store/models/app-state.model';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { ShoppingItem } from './store/models/shopping-item.model';
+import { AppState } from './store/models/app-state.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ngrx-shopping-list';
+export class AppComponent implements OnInit {
+  shoppingItems$: Observable<Array<ShoppingItem>>;
 
   constructor(private store: Store<AppState>) {
 
+  }
+
+  ngOnInit() {
+    this.shoppingItems$ = this.store.select(store => store.shopping);
   }
 }
